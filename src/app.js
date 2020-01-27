13161a472065a4313434636ae78831acdf587762
MarcinBalejko <mbbm9@o2.pl>
2020-01-27 17:16:51 +0100

    :white_check_mark: Added node-sass, added webpack-dev-server, corrected fetch request, data loads properly

--- src/app.js
+++ src/app.js
@@ -7,11 +7,13 @@ export class App {
     let self = this;
 
     $('.load-username').on('click', function (e) {
-      let userName = $('.username.input').val();
+      let userName = $('.enter-username').val();
 
       fetch('https://api.github.com/users/' + userName)
-        .then((response)=> {response.json})
-        .then(function (body) {
+        .then((response) => {
+          return response.json();
+        }).then(function (body) {
+          console.log(body)
           self.profile = body;
           self.update_profile();
         })
@@ -21,7 +23,7 @@ export class App {
   }
 
   update_profile() {
-    $('#profile-name').text($('.username.input').val())
+    $('#profile-name').text($('.enter-username').val())
     $('#profile-image').attr('src', this.profile.avatar_url)
     $('#profile-url').attr('href', this.profile.html_url).text(this.profile.login)
     $('#profile-bio').text(this.profile.bio || '(no information)')
